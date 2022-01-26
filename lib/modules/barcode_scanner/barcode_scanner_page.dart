442c84ada0745436d500b5a9d89f8aaad950e190
matheusmirandaferreira <matheusdemirandaferreira@gmail.com>
2022-01-26 17:01:55 -0300

    Corrigindo versão de pacotes e tela de leitor de QR Code

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/theme/app_colors.dart';
+import 'package:payflow/shared/theme/app_text_styles.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
-  BarcodeScannerPage({Key? key}) : super(key: key);
+  const BarcodeScannerPage({Key? key}) : super(key: key);
 
   @override
   State<BarcodeScannerPage> createState() => _BarcodeScannerPageState();
@@ -10,6 +12,37 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          'Escaneie o código de barras do boleto', 
+          style: TextStyles.buttonBackground,
+        ),
+        leading: BackButton(
+          color: AppColors.background,
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.black,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.transparent,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
