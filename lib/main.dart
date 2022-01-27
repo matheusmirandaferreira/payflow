886407388d4a92a953ec524b9197c2f8336002ef
matheusmirandaferreira <matheusdemirandaferreira@gmail.com>
2022-01-27 17:05:02 -0300

    Correção no leitor de qr code

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:camera/camera.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:payflow/app_widget.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    final cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print('Error: $e.code\nError Message: $e.message');
+  }
   runApp(AppFireBase());
 }
 
@@ -15,33 +22,26 @@ class AppFireBase extends StatefulWidget {
 
 class _AppFireBaseState extends State<AppFireBase> {
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-  
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future:_initialization,
-      builder: (context, snapshot) {
-        if(snapshot.hasError) {
-          return Material(
-            child: Center(
-              child: Text("Não foi possível inicializar o Firebase", 
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Material(
+                child: Center(
+                    child: Text(
+              "Não foi possível inicializar o Firebase",
               textDirection: TextDirection.ltr,
-              )
-            )
-          );
-        }
-
-        else if(snapshot.connectionState == ConnectionState.done) {
-          return AppWidget();
-        } else {
-        return Material(
-          child: 
-            Center(
-              child: CircularProgressIndicator()
-            ),
-          );
-        }
-      }
-    );
+            )));
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            return AppWidget();
+          } else {
+            return Material(
+              child: Center(child: CircularProgressIndicator()),
+            );
+          }
+        });
   }
 }
